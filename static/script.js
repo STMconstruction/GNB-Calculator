f6ae2799b4e0690efe2837bdd17311e5321db963
talgatsadbaev <talgat707@mail.ru>
2025-02-13 11:14:48 +0500

    Исправлены расчеты по таблице

--- static/script.js
+++ static/script.js
@@ -9,13 +9,32 @@ document.getElementById("calculateButton").addEventListener("click", function ()
         return;
     }
 
-    // Логика расчета стоимости
-    const pricePerMeter = pipeDiameter * 10;
+    // Логика расчета стоимости по таблице
+    let pricePerMeter = 0;
+
+    if (drillLength < 60) {
+        if (pipeDiameter <= 100) {
+            pricePerMeter = 15000;
+        } else if (pipeDiameter <= 200) {
+            pricePerMeter = 17000;
+        } else if (pipeDiameter <= 315) {
+            pricePerMeter = 18000;
+        }
+    } else {
+        if (pipeDiameter <= 100) {
+            pricePerMeter = 10000;
+        } else if (pipeDiameter <= 200) {
+            pricePerMeter = 12000;
+        } else if (pipeDiameter <= 315) {
+            pricePerMeter = 13000;
+        }
+    }
+
     const totalCost = drillLength * pricePerMeter;
 
     // Вывод результата
     const resultDiv = document.getElementById("result");
-    resultDiv.innerHTML = `<h3>Расчетная стоимость: ${totalCost} руб.</h3>`;
+    resultDiv.innerHTML = `<h3>Расчетная стоимость: ${totalCost.toLocaleString()} тг.</h3>`;
 
     // Отправка данных на сервер
     fetch("/submit", {
